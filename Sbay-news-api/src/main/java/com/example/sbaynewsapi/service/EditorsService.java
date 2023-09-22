9ba31ea1d8cc0df3096902906cda861d8f237f0a
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-09-22 15:36:55 +0700

    up

--- Sbay-news-api/src/main/java/com/example/sbaynewsapi/service/EditorsService.java
+++ Sbay-news-api/src/main/java/com/example/sbaynewsapi/service/EditorsService.java
@@ -1,7 +1,24 @@
 package com.example.sbaynewsapi.service;
 
+import com.example.sbaynewsapi.model.Editors;
+import com.example.sbaynewsapi.repository.IEditorsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EditorsService implements IEditorsService{
+    @Autowired
+    private IEditorsRepository iEditorsRepository;
+    @Override
+    public Page<Editors> getAll(String name, Pageable pageable) {
+        if (name.equals("null")){
+            return iEditorsRepository.getAll("",pageable);
+
+        }else {
+            return iEditorsRepository.getAll(name,pageable);
+
+        }
+    }
 }
